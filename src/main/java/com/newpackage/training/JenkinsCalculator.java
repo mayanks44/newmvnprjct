a5469876d5b024f921f86cce8858c805d2e9f855
mayank <mayank.saraswat@vuclip.com>
2020-01-19 23:11:54 +0530

    first commit all files upload

---
+++ src/main/java/com/newpackage/training/JenkinsCalculator.java
@@ -0,0 +1,13 @@
+package com.newpackage.training;
+
+public class JenkinsCalculator {
+    //Method to add 2 numbers
+    public int addNumbers(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    //Method 2 subtract 2 numbers
+    public int subtractNumbers(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+}
